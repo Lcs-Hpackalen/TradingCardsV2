b5789cef467c32a89aeb9468c6373fdce4fcc623
Hannu Packalen <hpackalen@lcs.on.ca>
2024-11-19 13:57:06 -0500

    Initial Commit

---
+++ TradingCardsV2/TradingCardsV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsV2App.swift
+//  TradingCardsV2
+//
+//  Created by Hannu Packalen on 2024-11-19.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
