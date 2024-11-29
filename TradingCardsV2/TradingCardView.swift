486433911702fb125be8981966e46777f07f3b81
Hannu Packalen <hpackalen@lcs.on.ca>
2024-11-28 19:34:25 -0500

    Made a file for the view that leads to detail card view.

---
+++ TradingCardsV2/TradingCardView.swift
@@ -0,0 +1,21 @@
+//
+//  TradingCardView.swift
+//  TradingCardsV2
+//
+//  Created by Hannu Packalen on 2024-11-28.
+//
+
+import SwiftUI
+
+struct TradingCardView: View {
+    //Mark:stored properties
+    
+    //Mark:computed properties
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TradingCardView()
+}
