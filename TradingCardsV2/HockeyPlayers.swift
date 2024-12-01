09d40641d5b8746ef2846bc813c885917d7cffc2
Hannu Packalen <hpackalen@lcs.on.ca>
2024-12-01 01:14:00 -0500

    changed the fun facts on some players

--- TradingCardsV2/HockeyPlayers.swift
+++ TradingCardsV2/HockeyPlayers.swift
@@ -22,11 +22,11 @@ struct HockeyPlayer: Identifiable{
     let playerPicture: String
     let funFact: String
 }
-let mitchMarner = HockeyPlayer(playerFirstName: "Mitch", playerLastName: "Marner", id: 1, position: "Winger", playernumber: "#16", shoots: "Right", points: 85, TOI: "21:17", signatureview: "mitchSignature", playerPicture:"mitchView", funFact: "Mitch won the Red Tilson Trophy, the Wayne Gretzky 99 Award, the Ed Chynoweth Trophy and was CHL Player of the Year, as well as leading his team to a Memorial Cup victory")
+let mitchMarner = HockeyPlayer(playerFirstName: "Mitch", playerLastName: "Marner", id: 1, position: "Winger", playernumber: "#16", shoots: "Right", points: 85, TOI: "21:17", signatureview: "mitchSignature", playerPicture:"mitchView", funFact: "Mitch won the Red Tilson Trophy, the Wayne Gretzky 99 Award, the Ed Chynoweth Trophy and was CHL Player of the Year")
 
 let austonMathews = HockeyPlayer(playerFirstName:"Auston", playerLastName: "Mathews", id: 2, position: "center", playernumber: "#34", shoots: "Left", points: 107, TOI: "20:49", signatureview: "austonSignature", playerPicture: "austonView", funFact: "Auston Mathews is the first player since 2008 to score 65+ goals in a season")
 
-let williamNylander = HockeyPlayer(playerFirstName: "William", playerLastName: "Nylander", id: 3, position: "Winger", playernumber: "#88", shoots: "Right", points: 98, TOI: "19:17", signatureview: "nylanderSignature", playerPicture: "nylanderView", funFact: "Nylander was born in Calgary while his father, Michael, was playing for the Calgary Flames. He is a dual citizen of Sweden and Canada. He spent the majority of his youth in the United States where his father played for several NHL teams.")
+let williamNylander = HockeyPlayer(playerFirstName: "William", playerLastName: "Nylander", id: 3, position: "Winger", playernumber: "#88", shoots: "Right", points: 98, TOI: "19:17", signatureview: "nylanderSignature", playerPicture: "nylanderView", funFact: "Nylander was born in Calgary while his father, Michael, was playing for the Calgary Flames. He is a dual citizen of Sweden and Canada.")
 
 let johnTavares = HockeyPlayer(playerFirstName: "John", playerLastName: "Tavares", id: 4, position: "center", playernumber: "#91", shoots: "Right", points: 65, TOI: "17:52", signatureview: "johnSignature", playerPicture: "johnView", funFact: "Tavares broke into the OHL after gaining exceptional player status at age 14, allowing the Generals to select him in the OHL Priority Draft as an underage player in 2005.")
  
